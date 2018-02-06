5bdd946ff4a8db2f0f4358e74907a6ec5eb9372e
Mahalo <zionbasque@gmail.com>
2018-02-05 17:50:27 -0700

    Delete evolver.py

--- evolver.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# Zion L. Basque
-# 2018-01-22
-# This python program attempts to trace different branches executed in an ELF
-# through jmp addresses called after a iomparision.
-
-
